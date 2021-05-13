6129ba1c4d5c4f36c8b0f1e93bc6af1ae9a18ce9
Mattea Swain <mmswain@tql.com>
2021-05-13 14:26:55 -0400

    Noah Update

---
+++ Slide-139-update-noah.sql
@@ -0,0 +1,25 @@
+-- Add new student info for Noah Phence
+/*
+insert into Student (Firstname,Lastname,StateCode,SAT,GPA,MajorId)
+	values
+		(	
+			'Noah', 'Phense', 'GA', 1235, 3.15,
+			(
+				select ID from Major
+					where Code = 'MATH'
+			)
+		);
+*/
+/*
+update Student set
+	GPA = 3.51, MajorId = 
+		(
+			select Id from Major
+				where code = 'ACCT'
+		)
+			where Firstname = 'Noah'and Lastname = 'Phence';
+*/
+
+
+
+
