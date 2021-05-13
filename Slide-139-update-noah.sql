5c50d84c285235241b0911fcc095dad00ff36cb6
Mattea Swain <mmswain@tql.com>
2021-05-13 14:35:48 -0400

    Updated Naoh activity

--- Slide-139-update-noah.sql
+++ Slide-139-update-noah.sql
@@ -12,13 +12,20 @@ insert into Student (Firstname,Lastname,StateCode,SAT,GPA,MajorId)
 */
 /*
 update Student set
-	GPA = 3.51, MajorId = 
-		(
-			select Id from Major
-				where code = 'ACCT'
-		)
-			where Firstname = 'Noah'and Lastname = 'Phence';
+	GPA = 3.51, 
+		MajorId = 
+			(
+				select Id from Major
+					where code = 'ACCT'
+			)
+						where Firstname = 'Noah'and Lastname = 'Phence';
 */
+
+/*
+delete
+	from Student
+		where Firstname = 'Noah';
+/*
 
 
 
