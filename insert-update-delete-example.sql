3e62a33a32b4a5f05d1637d33c181d9ea71f344a
Mattea Swain <mmswain@tql.com>
2021-05-13 13:46:10 -0400

    Before lunch examples

--- insert-update-delete-example.sql
+++ insert-update-delete-example.sql
@@ -6,18 +6,19 @@
 	('ACME MFG 2CO', 'Cincinnati', 'OH', 1000, 1)
 
 	update Customers set
-	name = 'Acme Mfg Co 0',
+	name = 'Acme Mfg Co 2',
 	Sales = 0
-	where Name = 'ACME MFG CO';
+	where Name = 'ACME MFG CO 2';
 
 	
 	select * from Customers
-		where Name = 'Acme Mfg Co 0'
+		where Name like 'ACME%';
 
 	
 	Delete from Customers
-	where Id = 37
+	where Id = (41)
 
 	update Customers set 
 	name = 'ACME MFG CO 2'
 	where Name = 'ACME MFG 2CO';
+
