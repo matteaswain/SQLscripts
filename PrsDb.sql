0ae6090faf83b3d75da352678b44755c4ab31d00
Mattea Swain <mmswain@tql.com>
2021-05-14 14:05:54 -0400

    Weekend Homework

--- PrsDb.sql
+++ PrsDb.sql
@@ -56,3 +56,46 @@ create table Requests
 							Total decimal(11,2) not null default 0,
 								UserId int not null foreign key references Users(Id)
 	);
+
+	create table RequestLines
+		(Id int primary key identity(1,1),
+			RequestId int not null Foreign key references Requests(Id),
+				ProductId int not null Foreign key references Products(Id),
+					Quantity int not null default 1
+
+		);
+Go
+
+-- insert 3 users, 3 venders, 5 products < $50
+
+insert into Users
+	(Username, Password, Firstname, Lastname, Phone, Email, IsReviewer, IsAdmin)
+		values ('Admin', 'adminlife22' ,'Admin', 'Admin', 8888888888, 'admin@fakeemail.com',1,1);
+
+insert into Users
+	(Username, Password, Firstname, Lastname,IsReviewer)
+		values ('Mimi', 'Mimirocks', 'Mia', 'Ausa', 1);
+
+insert into Users
+	(Username, Password, Firstname, Lastname)
+		values ('Chuck', 'Chuckischessy','Chuck','Taylor');
+
+Go
+
+insert into Vendors
+	(Code, Name,Address,City, State, Zip)
+		values ('TRG', 'Target','222 Target Rd','Denver', 'CO','12345');
+
+insert into Vendors
+	(Code, Name,Address,City, State, Zip)
+		values ('STPLS', 'Staples','444 Staple Ave','Spokan Valley', 'WA', '67890');
+
+insert into Vendors
+	(Code, Name,Address,City, State, Zip)
+		values ('AMZ', 'Amazon','5354 Amazon Forest','Stockton', 'CA', '13579');
+Go
+
+insert into Products
+	(PartNbr, Name, Price, VendorId)
+		values('PPR','Paper', 1.99, 1); 
+Go
